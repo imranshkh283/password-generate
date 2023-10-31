6828a84b879a9c88a26c3c3cd8824ff630bf4447
Imran <imranshkh283@gmail.com>
2023-11-01 01:09:46 +0530

    main script

---
+++ script.js
@@ -0,0 +1,7 @@
+const btnGenerate = document.getElementById("btn-generate");
+const txtPassword = document.querySelector(".password");
+const pwdLength = document.getElementById("password-length-number");
+const pwdlowerCase = document.getElementById("lowercase");
+const pwdUpperCase = document.getElementById("uppercase");
+const pwdNumbers = document.getElementById("numbers");
+const pwdSymbols = document.getElementById("symbols");
